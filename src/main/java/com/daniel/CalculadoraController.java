8d47843b50d9b27969a2da2ca2823bf28b5fed77
gabri <gabri@DESKTOP-1QVV0Q1>
2021-05-24 17:43:32 -0500

    2

--- src/main/java/com/daniel/CalculadoraController.java
+++ src/main/java/com/daniel/CalculadoraController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class CalculadoraController {
 
 	//@GetMapping("/calculadora/index")
-	@GetMapping(path = {"/calculadora", "/"})
+	@GetMapping(path = {"calculadora", "/"})
 	public String index(Model modelo) 
 	{
 		modelo.addAttribute("calcu", new Calculadora());
